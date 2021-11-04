cc3cfc81beb7940fab8b90c20652e1d979bd76c4
root <root@vps-a718f4ab.vps.ovh.net>
2021-11-04 12:38:03 +0000

    bug fix

--- back/src/Object/registry.py
+++ back/src/Object/registry.py
@@ -112,7 +112,7 @@ class registry:
         if end is True:
             return self.data()['open']['main'] == True
         if self.data()['open']['main'] == True:
-            return [True, {}, None]:
+            return [True, {}, None]
         return [False, "Repository is closed to external connection", 401]
 
     def create(self, name, creator, actions, roles, open = False):
--- back/src/Object/user_registry.py
+++ back/src/Object/user_registry.py
@@ -115,7 +115,7 @@ class user_registry:
         if not roles[0]:
             return [False, "Error in finding user default role", 500]
         roles = roles[1]['roles']
-        open =  self.reg.is_open():
+        open =  self.reg.is_open()
         if not open[0]:
             return open
         return self.add_user(id_user, roles)
