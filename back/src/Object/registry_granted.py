7320673e387eb734154c64ea208fb74a01df9ec5
SCcagg5 <sccagg5@gmail.com>
2022-02-11 00:31:30 +0100

    adding

--- back/src/Object/registry_granted.py
+++ back/src/Object/registry_granted.py
@@ -138,9 +138,10 @@ class registry_granted:
             &
             (r.row['date']['start'] <= now & r.row['date']['end'] >= now)
         ).order_by(r.desc(r.row['date']['start'])).group('registry_id').run())
-        invalid = dict(self.red.filter(
+        invalid = self.red.filter(
             (r.row["user_id"] == user_id)
             &
             (r.row['date']['start'] <= now & r.row['date']['end'] >= now)
-        ).order_by(r.desc(r.row['date']['start'])).group('registry_id', 'manual_validation').run())
+        ).order_by(r.desc(r.row['date']['start'])).group('registry_id', 'manual_validation').run()
+        invalid = {i[0] + '_' + i[1]: invalid[i] for i in invalid}
         return [True, {'logs_active': valid, 'logs_inactive': invalid}, None]
